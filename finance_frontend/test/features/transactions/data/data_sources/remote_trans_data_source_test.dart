f62a2e76c32e06e827f7aec597d896601b3a26e5
Kaleb Tesfahun <kalebtesfahun@gmail.com>
2025-12-01 12:56:55 +0300

    setup for deleteTransaction test same style as createTransferTransaction

--- finance_frontend/test/features/transactions/data/data_sources/remote_trans_data_source_test.dart
+++ finance_frontend/test/features/transactions/data/data_sources/remote_trans_data_source_test.dart
@@ -366,6 +366,132 @@ void main() {
         );
       }
     },);
+
+    group("tests for deleteTransferTransaction", () {
+      test(
+        "createTransferTransaction - success - return's the two transaction's",
+        () async {
+          // Arrange
+          when(
+            () => mockStorage.readString(key: "access_token"),
+          ).thenAnswer((_) async => "fake_acc");
+          when(() => mockNetwork.send(any())).thenAnswer(
+            (_) async => ResponseModel(
+              statusCode: 201,
+              headers: {},
+              body: jsonEncode({
+                "outgoing_transaction": fakeTransactionJson(
+                  id: 1,
+                  accountId: 1,
+                  transferGroupId: "fake_transfer_id",
+                  type: TransactionType.TRANSFER,
+                  isOutGoing: true,
+                ),
+                "incoming_transaction": fakeTransactionJson(
+                  id: 2,
+                  accountId: 2,
+                  transferGroupId: "fake_transfer_id",
+                  type: TransactionType.TRANSFER,
+                  isOutGoing: false,
+                ),
+              }),
+            ),
+          );
+
+          // Act
+          final transDs = container.read(transDataSourceProvider);
+          final (outgoing, incoming) = await transDs.createTransferTransaction(
+            fakeTransferTransactionCreate(),
+          );
+
+          // Assert
+          expect(outgoing.id, "1");
+          expect(incoming.id, "2");
+          expect(outgoing.isOutGoing, true);
+          expect(incoming.isOutGoing, false);
+          expect(outgoing.accountId, "1");
+          expect(incoming.accountId, "2");
+          expect(outgoing.transferGroupId, "fake_transfer_id");
+          expect(incoming.transferGroupId, "fake_transfer_id");
+
+          // verify the dependencies method's have been called with proper input's
+          verify(() => mockStorage.readString(key: "access_token")).called(1);
+          verify(
+            () => mockNetwork.send(
+              any(
+                that: isA<RequestModel>()
+                    .having((r) => r.method, "RestApi method", "POST")
+                    .having(
+                      (r) => r.url.toString(),
+                      "transaction's url",
+                      contains("/transfer"),
+                    ),
+              ),
+            ),
+          );
+
+          verifyNoMoreInteractions(mockStorage);
+          verifyNoMoreInteractions(mockNetwork);
+        },
+      );
+
+      /// create transaction error test's
+      final scenarios = [
+        TransactionErrorScenario(
+          statusCode: 400,
+          code: "INVALID_AMOUNT",
+          expectedException: InvalidInputtedAmount,
+        ),
+        TransactionErrorScenario(
+          statusCode: 400,
+          code: "INSUFFICIENT_BALANCE",
+          expectedException: AccountBalanceTnsufficient,
+        ),
+        TransactionErrorScenario(
+          statusCode: 400,
+          code: "Error",
+          expectedException: CouldnotCreateTransferTransaction,
+        ),
+      ];
+
+      for (TransactionErrorScenario s in scenarios) {
+        test(
+          "createTransferTransaction - non 201 :${s.code} - throws ${s.expectedException.toString()}",
+          () async {
+            // Arrange
+            when(
+              () => mockStorage.readString(key: "access_token"),
+            ).thenAnswer((_) async => "fake_acc");
+            when(() => mockNetwork.send(any())).thenAnswer(
+              (_) async => ResponseModel(
+                statusCode: s.statusCode,
+                headers: {},
+                body: jsonEncode({
+                  "detail": {"code": s.code},
+                }),
+              ),
+            );
+
+            final transDs = container.read(transDataSourceProvider);
+
+            Object typeMatcher;
+            typeMatcher = isA<CouldnotCreateTransaction>();
+            if (s.expectedException == InvalidInputtedAmount) {
+              typeMatcher = isA<InvalidInputtedAmount>();
+            }
+            if (s.expectedException == AccountBalanceTnsufficient) {
+              typeMatcher = isA<AccountBalanceTnsufficient>();
+            }
+
+            // Act & Assert
+            expect(
+              () => transDs.createTransaction(fakeTransactionCreate()),
+              throwsA(typeMatcher),
+            );
+          },
+        );
+      }
+    },);
   });
 
 
